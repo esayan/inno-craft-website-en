69d283fe093b25188dba6030b457745bf9cb512b
esayan <esayan@gmail.com>
2025-11-04 17:49:54 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-
-
+// vite.config.(js|ts)
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
-  build: { copyPublicDir: true },   // açıkça söyleyelim
+  base: '/',                 // custom domain
+  build: { copyPublicDir: true },
   plugins: [react()],
 })
