7850c9679d3c4a8fe349771f7ff729c753d94ceb
esayan <esayan@gmail.com>
2025-11-05 17:01:36 +0300

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,25 +1,13 @@
-import Billing from './Billing'
-import Business from './Business'
-import CardDeal from './CardDeal'
-import Clients from './Clients'
-import CTA from './CTA'
-import Feedback from './Feedback'
-import Footer from './Footer'
-import Hero from './Hero'
-import Navbar from './Navbar'
-import Stats from './Stats'
-import Testimonials from './Testimonials'
+import Business from "./Business";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
 
 export {
-    Billing,
-    Business,
-    CardDeal,
-    Clients,
-    CTA,
-    Feedback,
-    Footer,
-    Hero,
-    Navbar,
-    Stats,
-    Testimonials
-}
+  Business,
+  Footer,
+  Hero,
+  Navbar,
+  Stats,
+};
