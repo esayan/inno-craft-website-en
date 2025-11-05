1a273a07ed4158cb9a37788928999df69b921314
esayan <esayan@gmail.com>
2025-11-05 17:05:55 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,21 +5,22 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   const navLinks = [
-    { id: "home", title: "Home" },
-    { id: "services", title: "Services" },
-    { id: "solutions", title: "Solutions" },
-    { id: "clients", title: "Clients" },
-    { id: "contact", title: "Contact" },
+    { id: "home",     title: "Home",          href: "#home" },
+    { id: "services", title: "Our Services",  href: "#services" },
+    { id: "about",    title: "Who we are?",   href: "#about" },
+    // Contact Us tıklanınca e-posta açılır
+    { id: "contact",  title: "Contact Us",    href: "mailto:esayan@inno-craft.com" },
   ];
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       {/* Logo */}
-  <img
-  src="/IC_logo.png"
-  alt="Inno-Craft"
-  className="w-[180px] sm:w-[200px] md:w-[220px] h-auto object-contain"
-/>
+      <img
+        src="/IC_logo.png"
+        alt="Inno-Craft"
+        className="w-[180px] sm:w-[200px] md:w-[220px] h-auto object-contain"
+      />
+
       {/* Desktop Menu */}
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, i) => (
@@ -29,7 +30,7 @@ const Navbar = () => {
               i === navLinks.length - 1 ? "mr-0" : "mr-10"
             }`}
           >
-            <a href={`#${nav.id}`}>{nav.title}</a>
+            <a href={nav.href}>{nav.title}</a>
           </li>
         ))}
       </ul>
@@ -46,9 +47,9 @@ const Navbar = () => {
         <div
           className={`${
             toggle ? "flex" : "hidden"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[160px] rounded-xl sidebar`}
         >
-          <ul className="list-none flex flex-col justify-end items-center flex-1">
+          <ul className="list-none flex flex-col justify-end items-start flex-1">
             {navLinks.map((nav, i) => (
               <li
                 key={nav.id}
@@ -56,7 +57,9 @@ const Navbar = () => {
                   i === navLinks.length - 1 ? "mb-0" : "mb-4"
                 }`}
               >
-                <a href={`#${nav.id}`}>{nav.title}</a>
+                <a href={nav.href} onClick={() => setToggle(false)}>
+                  {nav.title}
+                </a>
               </li>
             ))}
           </ul>
