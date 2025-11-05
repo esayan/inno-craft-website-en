ec71d7783e7e48ec2281cc5e94831afe0a727128
esayan <esayan@gmail.com>
2025-11-05 19:32:12 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',   // custom domain + alt path senaryolarında güvenli
+  base: '/', // ✅ custom domain (ör. inno-craft.com) için doğru ayar
 })
